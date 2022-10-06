f4813ea8fff8f7f39423ccf88f737c059eba3d4c
Fitria Zusni Farida <98293694+FiZufa@users.noreply.github.com>
2022-10-06 12:39:48 +0700

    Update C.java

--- Lab0/C.java
+++ Lab0/C.java
@@ -15,23 +15,23 @@ public class C {
     }
 
     public static long ways(int x1, int y1, int x2, int y2, int M){
-        int x = x2-x1;
-        int y = y2-y1;
-        long[][] arr = new long[x+1][y+1];
+        int x = x2-x1; // (1) --> O(1)
+        int y = y2-y1; // (1) --> O(1)
+        long[][] arr = new long[x+1][y+1]; //(2) --> O(1)
 
-        for(int i=0 ; i<=x ; i++){
+        for(int i=0 ; i<=x ; i++){ // (3) --> O(n)
             arr[i][0] = 1 ;
         }
-        for(int i=0 ; i<=y ; i++){
+        for(int i=0 ; i<=y ; i++){ //(4) --> O(n)
             arr[0][i] = 1 ;
         }
-        for(int i=1 ; i<=x ; i++){
+        for(int i=1 ; i<=x ; i++){ // (5)&(6) -->O(n^2)
             for(int j=1 ; j<=y ; j++){
-                arr[i][j] = (arr[i-1][j] + arr[i][j-1]) % M ;
+                arr[i][j] = (arr[i-1][j] + arr[i][j-1]) % M ; //(7)
             }
         }
 
-        return arr[x][y] ;
+        return arr[x][y] ; //(8)
     }
 
 }
